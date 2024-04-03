6a4e18ea695265f959bc757cc12ef8eee06d78c2
checkandmate9 <bilalsmv09@gmail.com>
2024-04-03 17:14:04 +0300

    cps (click per second) eklendi

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ let seconds = 0;
 const duration = 5;
 let timerStarted = false;
 let milliseconds = 0;
+let intervalID; 
+let cps;
+
+function startCalculatingCPS() {
+  intervalID = setInterval(calculateCPS, 1000);
+}
+
+function calculateCPS() {
+  if (seconds >= 5) {
+    clearInterval(intervalID);
+  } else {
+    if (timerStarted) {
+      cps = clickCount / 5;
+      console.log(cps);
+    }
+  }
+}
+
+startCalculatingCPS();
 
 
 function santisaniye(){
@@ -32,29 +51,35 @@ if(seconds>=duration){
   };
   let modalMessage = "";
   let modalMessage2 = ``;
+  let modalMessage3 = ``;
   openModal();
   if(clickCount >= 1 && clickCount <=20){
      document.getElementById("turtleimg").style.display = "block";
      modalMessage = "You are a turtle, get your lazy ass up!!";
      modalMessage2 = `You have clicked ${clickCount} times in ${duration} second!!`;
+     modalMessage3 = `Your CPS(cilck per second) is ${cps}`
   }
   else if (clickCount >= 21 && clickCount <=40){
     document.getElementById("rabbitimg").style.display = "block";
     modalMessage = "You are a rabbit, looks like you've got some finger there! ";
     modalMessage2 = `You have clicked ${clickCount} times in ${duration} second!!`;
+    modalMessage3 = `Your CPS(cilck per second) is ${cps}`
   }
   else if (clickCount >= 41 &&  clickCount <= 57){
     document.getElementById("monkeyimg").style.display = "block";
     modalMessage = "You are a monkey, perhaps? do monkeys have fast fingers??";
     modalMessage2 = `You have clicked ${clickCount} times in ${duration} second!!`;
+    modalMessage3 = `Your CPS(cilck per second) is ${cps}`
   }
   else{
     document.getElementById("cheetahimg").style.display = "block";
     modalMessage = "You are a cheetah, your lover must love you very bad!! ;)";
     modalMessage2 = `You have clicked ${clickCount} times in ${duration} second!!`;
+    modalMessage3 = `Your CPS(cilck per second) is ${cps}`
   }
   document.getElementById("modalMessage").textContent = modalMessage;
   document.getElementById("modalMessage2").textContent = modalMessage2;
+  document.getElementById("modalMessage3").textContent = modalMessage3;
 }
 }
 },1000);
